1e6181b39c3bd86621d32f97f88a381dd3ff0abe
Benjamin Contreras <benja.contreras732@gmail.com>
2023-04-30 04:01:41 -0400

    Boundary a medias

--- Assets/C# Scripts/CameraFollow.cs
+++ Assets/C# Scripts/CameraFollow.cs
@@ -16,21 +16,49 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AspectRatioBoxChange();
+        FollowPlayer();
         
     }
 
     void AspectRatioBoxChange()
     {
-        if(Camera.main.aspect >= (1.6f) && Camera.main.aspect < 1.7f)
+        if (Camera.main.aspect >= (1.6f) && Camera.main.aspect < 1.7f)
         {
-            cameraBox.size = new Vector2(23,14.3f);
+            cameraBox.size = new Vector2(23, 14.3f);
         }
-        if(Camera.main.aspect >= (1.7f) && Camera.main.aspect < (1.8f))
+        if (Camera.main.aspect >= (1.7f) && Camera.main.aspect < (1.8f))
         {
             cameraBox.size = new Vector2(25.47f, 14.3f);
         }
-        //if (Camera.main.aspect >= (1.25f) && Camera.main.aspect < ())
-        
+        if (Camera.main.aspect >= (1.25f) && Camera.main.aspect < (1.3f))
+        {
+            cameraBox.size = new Vector2(18f, 14.3f);
+        }
+        if (Camera.main.aspect >= (1.3f) && Camera.main.aspect < 1.4f)
+        {
+            cameraBox.size = new Vector2(19.13f, 14.3f);
+        }
+        if (Camera.main.aspect >= (1.5) && Camera.main.aspect < 1.6f)
+        {
+            cameraBox.size = new Vector2(21.6f, 14.3f);
+        }
+        //float cameraH = (Camera.main.orthographicSize * 2) + 0.1f;
+
+        //float cameraW = (Camera.main.orthographicSize * Camera.main.aspect * 2) + 0.1f;
+
+        //cameraBox.size = new Vector2(cameraW, cameraH);
+
+    }
+
+    void FollowPlayer ()
+    {
+        if (GameObject.Find("Boundary"))
+        {
+            transform.position = new Vector3(Mathf.Clamp(player.position.x, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.x + cameraBox.size.x / 2, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.x - cameraBox.size.x / 2),
+                                            Mathf.Clamp(player.position.y, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.min.y + cameraBox.size.y / 2, GameObject.Find("Boundary").GetComponent<BoxCollider2D>().bounds.max.y - cameraBox.size.y / 2),
+                                            transform.position.z);
+        }
     }
 
 }
